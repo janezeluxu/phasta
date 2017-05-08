a25fd71fd32d0906a26db6718a868a31b635c65e
zeluxu <janezeluxu@gmail.com>
2017-05-08 09:50:47 -0400

    shapefunction

--- shapeFunction/src/uniformP.c
+++ shapeFunction/src/uniformP.c
@@ -1,24 +1,32 @@
 #include "shapeFuncInternals.h"
-
+#include <math.h>
+#include <stdio.h>  
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+int factorial(int n)
+{
+    if(n > 1)
+        return n * factorial(n - 1);
+    else
+        return 1;
+}
+
   /* Calculate shape functions and derivative for tet elements */
 int TetShapeAndDrv(int p,double par[3],double N[],double dN[][3]) {
-  static int TetEMAP[6][2]={{0,1},{1,2},{2,0},{0,3},{1,3},{2,3}};
-  static int TetFMAP[4][3]={{0,1,2},{0,3,1},{1,3,2},{0,2,3}};
-  int NS,is,i,j,ip,nshp=0,(*fpdrv)[2];
-  double L[4],Le[2],Lf[3],mode,blend,bdrv[3],mdrv[3],epdrv[3][2],mfdrv[2];
-  double tmp,rst,rsw,stw,rstw,rtw;
+  int i,j,nshp=0;
+  double L[4];
   if(p<1)
     return nshp;
   L[0]=par[0];
   L[1]=par[1];
   L[2]=par[2];
   L[3]=1.0e0-par[0]-par[1]-par[2];
-  /* collect all vertex modes */
-  for(i=0; i <4; i++) {
+  
+  if (p==1) {
+	  printf("tetp1 = %d\n", p);
+    for(i=0; i <4; i++) {
     N[i] = L[i];
     if(i==3) 
       dN[i][0]=dN[i][1]=dN[i][2]=-1.0e0;
@@ -32,77 +40,56 @@ int TetShapeAndDrv(int p,double par[3],double N[],double dN[][3]) {
     }
   }
   nshp=4;
-  if( p > 1 ) {
-    /* collect all edge modes, for all p */
-    for(i=0; i <6; i++) {
-      Le[0]=L[TetEMAP[i][0]];
-      Le[1]=L[TetEMAP[i][1]];
-      blend = R_edgeBlendTet(TetEMAP[i],L);
-      R_edgeBlendTetDrv(TetEMAP[i],L,bdrv);
-      E_parDrv(TetEMAP[i][0],TetEMAP[i][1],Stet,epdrv);
-      for(ip=2; ip <= p; ip++) {
-	mode = E_modeShape(ip,Le);
-	E_modeShapeDrv(ip, Le, mdrv);
-	N[nshp] = blend*mode;
-	dN[nshp][0] = bdrv[0]*mode + 
-	        blend*(mdrv[0]*epdrv[0][0]+mdrv[1]*epdrv[0][1]);
-        dN[nshp][1] = bdrv[1]*mode + 
-	        blend*(mdrv[0]*epdrv[1][0]+mdrv[1]*epdrv[1][1]);
-        dN[nshp][2] = bdrv[2]*mode + 
-	        blend*(mdrv[0]*epdrv[2][0]+mdrv[1]*epdrv[2][1]);
-	nshp++;
-      }
-    }
-  }
-  if( p > 2 ) {
-    /* collect all face modes for all p */
-    for(i=0; i <4; i++) {
-      Lf[0]=L[TetFMAP[i][0]];
-      Lf[1]=L[TetFMAP[i][1]];
-      Lf[2]=L[TetFMAP[i][2]];
-      blend=Lf[0]*Lf[1]*Lf[2];
-      R_faceBlendTetDrv(TetFMAP[i],L,bdrv);
-      F_parDrv(TetFMAP[i][0],TetFMAP[i][1],TetFMAP[i][2],Stet,&fpdrv);
-      for(ip=3; ip <= p; ip++) {
-	NS = ip-2;
-	for(is=0; is < NS; is++) {
-	  mode = F_modeShapeTri(ip,is,Lf);
-	  N[nshp] = blend*mode;
-	  F_modeShapeTriDrv(ip,is,Lf,mfdrv);
-	  mdrv[0]=mfdrv[0]*(double)fpdrv[0][0]+ mfdrv[1]*(double)fpdrv[0][1];
-	  mdrv[1]=mfdrv[0]*(double)fpdrv[1][0]+ mfdrv[1]*(double)fpdrv[1][1];
-	  mdrv[2]=mfdrv[0]*(double)fpdrv[2][0]+ mfdrv[1]*(double)fpdrv[2][1];
-	  dN[nshp][0] = bdrv[0]*mode + blend*mdrv[0] ;      
-	  dN[nshp][1] = bdrv[1]*mode + blend*mdrv[1] ;      
-	  dN[nshp][2] = bdrv[2]*mode + blend*mdrv[2] ;
-	  nshp++;
-	}
-      }
-    }
-  }
-  if( p > 3 ) {
-    /* collect all region modes */
-    for(ip=4; ip <= p; ip++) {
-      NS = (ip-3)*(ip-2)/2 ;
-      blend = L[0]*L[1]*L[2]*L[3];
-      tmp = 1.0e0-L[0]-L[1]-L[2] ;
-      rsw = L[0]*L[1]*tmp;
-      stw = L[1]*L[2]*tmp ;
-      rtw = L[0]*L[2]*tmp ;
-      rst = L[0]*L[1]*L[2] ;
-      rstw = rst*L[3] ;
-      for(is=0; is < NS; is++) {
-	mode = R_modeShapeTet(ip,is,L);
-	N[nshp] = blend*mode;
-	R_modeShapeTetDrv(ip, is, L, mdrv);
-	dN[nshp][0] = mode*(stw-rst) + rstw*mdrv[0] ; 
-	dN[nshp][1] = mode*(rtw-rst) + rstw*mdrv[1] ;
-	dN[nshp][2] = mode*(rsw-rst) + rstw*mdrv[2] ;
-	nshp++;
-      }
-    }
-  }
   return nshp;
+}
+  if (p>1){
+  int a[10][4];
+  
+  printf("tetp2 = %d\n", p);
+
+  for (i=0; i<10; i++){
+    for (j=0; j<4; j++)
+    {
+      a[i][j]=0;
+    }
+  }
+  a[0][0]=2;a[1][1]=2;a[2][2]=2;a[3][3]=2;
+  a[4][0]=1;a[4][2]=1;a[5][1]=1;a[5][2]=1;
+  a[6][0]=1;a[6][1]=1;a[7][0]=1;a[7][3]=1;
+  a[8][2]=1;a[8][3]=1;a[9][1]=1;a[9][3]=1;
+  //printf("a[9][0]%d\n", a[9][0]); 
+  
+  
+  /* collect all vertex modes */
+  for(i=0; i <10; i++) {
+          //printf("factorial%f, i=%d\n", factorial(a[i][0]),i);
+          int tmp0 = a[i][0];
+          int fac0 = factorial(tmp0);
+          int tmp1 = a[i][1];
+          int fac1 = factorial(tmp1);
+          int tmp2 = a[i][2];
+          int fac2 = factorial(tmp2);
+          int tmp3 = a[i][3];
+          int fac3 = factorial(tmp3);
+
+          //printf("tmp = %d,fac = %d, i=%d\n",tmp0, fac0,i);
+
+	  double coef = 2.0/(fac0*fac1*fac2*fac3);
+	  //printf("coef%f, i = %d\n", coef,i); 
+	  N[i] = coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*pow(L[2],a[i][2])*pow(L[3],a[i][3]);
+	  //printf("shapefunction%f\n", N[i]); 
+	  dN[i][0] = coef*a[i][0]*pow(L[0],a[i][0]-1)*pow(L[1],a[i][1])*pow(L[2],a[i][2])*pow(L[3],a[i][3])\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*pow(L[2],a[i][2])*a[i][3]*pow(L[3],a[i][3]-1);
+	  dN[i][1] = coef*pow(L[0],a[i][0])*a[i][1]*pow(L[1],a[i][1]-1)*pow(L[2],a[i][2])*pow(L[3],a[i][3])\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*pow(L[2],a[i][2])*a[i][3]*pow(L[3],a[i][3]-1);			
+	  dN[i][2] = coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*a[i][2]*pow(L[2],a[i][2]-1)*pow(L[3],a[i][3])\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*pow(L[2],a[i][2])*a[i][3]*pow(L[3],a[i][3]-1);
+      	  //printf("shapefunctionDiv%f\n", dN[i][0]);
+
+  }
+  nshp=10;
+  return nshp;
+}
 }
 
 /* calculate the shape functions and their derivatives for
@@ -113,16 +100,16 @@ int TriShapeAndDrv(int p,double par[2],double N[],double dN[][2]){
   int i,j,nshp=0;
   double L[3];
   
-  if(p > 2) /* not supported */
+  if(p<1)
     return nshp;
+    
   L[0]=par[0];
   L[1]=par[1];
   L[2]=1.0e0-par[0]-par[1];
     
-  /* define shape functions for a quadratic triangle */
-
-  /* collect all vertex modes */
-  for(i=0; i<3; i++) {
+   if(p==1) {
+	   printf("Trip = %d\n", p);
+      for(i=0; i<3; i++) {
     N[i] = L[i];
     if(i==2)
       dN[i][0]=dN[i][1]=-1.0e0;
@@ -136,20 +123,49 @@ int TriShapeAndDrv(int p,double par[2],double N[],double dN[][2]){
     }
   }
   nshp=3;
-  if( p > 1 ){
-    /* collect edge modes (only quadratic for now) */
-    N[3] = -2.0e0*L[0]*L[1];
-    N[4] = -2.0e0*L[1]*L[2];
-    N[5] = -2.0e0*L[0]*L[2];
-    dN[3][0] = -2.0e0*L[1];
-    dN[3][1] = -2.0e0*L[0];
-    dN[4][0] = 2.0e0*L[1];
-    dN[4][1] = -2.0e0+2.0e0*L[0]+4.0e0*L[1];
-    dN[5][0] = -2.0e0+4.0e0*L[0]+2.0e0*L[1];
-    dN[5][1] = 2.0e0*L[0];
-    nshp=6;
-  }
   return nshp;
+}
+if(p>1) {
+	printf("Trip = %d\n", p);
+    int a[6][3];
+  
+  for (i=0; i<6; i++)
+    for (j=0; j<3; j++)
+    {
+      a[i][j]=0;
+    }
+
+  a[0][0]=2;a[1][1]=2;a[2][2]=2;
+  a[3][0]=1;a[3][2]=1;a[4][1]=1;a[4][2]=1;
+  a[5][0]=1;a[5][1]=1;
+  //std::cout <<" a[0][] "<<a[0][0]<< '\n';
+  
+  
+  /* collect all vertex modes */
+  for(i=0; i <6; i++) {
+	  int tmp0 = a[i][0];
+          int fac0 = factorial(tmp0);
+
+          int tmp1 = a[i][1];
+          int fac1 = factorial(tmp1);
+          int tmp2 = a[i][2];
+          int fac2 = factorial(tmp2);
+
+          double coef = 2.0/(fac0*fac1*fac2);
+
+	  //std::cout <<" coef of SF "<<coef<< '\n';
+	  N[i] = coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*pow(L[2],a[i][2]);
+	  //std::cout <<" SF "<<N[i]<< '\n';
+	  dN[i][0] = coef*a[i][0]*pow(L[0],a[i][0]-1)*pow(L[1],a[i][1])*pow(L[2],a[i][2])\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*a[i][2]*pow(L[2],a[i][2]-1);
+	  dN[i][1] = coef*pow(L[0],a[i][0])*a[i][1]*pow(L[1],a[i][1]-1)*pow(L[2],a[i][2])\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*a[i][2]*pow(L[2],a[i][2]-1);			
+	  dN[i][2] = coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*a[i][2]*pow(L[2],a[i][2]-1)\
+					-coef*pow(L[0],a[i][0])*pow(L[1],a[i][1])*a[i][2]*pow(L[2],a[i][2]-1);
+  }
+  nshp=6;
+  return nshp;
+}
 }
     
     
